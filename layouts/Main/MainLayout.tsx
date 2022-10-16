264788ad824e199be994cf9245380711ab14854b
Mari Komardina <fox_kom@internet.ru>
2022-10-16 20:03:14 +0300

    feat: add footer

--- layouts/Main/MainLayout.tsx
+++ layouts/Main/MainLayout.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import MemoNavBar from "../Header/NavBar";
 import { Child } from "../../types/HeaderLinks";
 import CardsCourse from "../../components/courses/CardsCourse";
+import Footer from "../../components/footer/Footer";
 
 const MainLayout: React.FC<Child> = ({children})=>{
   return (
       <>
         <CardsCourse />
+        <Footer />
       </>
   );
 };
